23d4c05d8b5123d2babc5ff93a991e5b53dcfc7c
vibhuthespeedst <128738942+vibhuthespeedst@users.noreply.github.com>
2024-09-29 19:44:55 +0530

    Intial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,135 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Lottie from 'lottie-react';
+import loadingAnimation from './loading.json';
 
 function App() {
+  const [data, setData] = useState({});
+  const [loc, setLoc] = useState("");
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(true);
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${loc}&units=imperial&appid=b8beb8bcdd794dff7a9e3ad0ad1d232f`;
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const { latitude, longitude } = position.coords;
+        axios
+          .get(
+            `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=imperial&appid=b8beb8bcdd794dff7a9e3ad0ad1d232f`
+          )
+          .then((response) => {
+            setData(response.data);
+            setError(false);
+          })
+          .catch((error) => {
+            setError(true);
+          })
+          .finally(() => {
+            setLoading(false);
+          });
+      },
+      (error) => {
+        setError(true);
+        setLoading(false);
+      }
+    );
+  }, []);
+
+  const searchLocation = () => {
+    setLoading(true);
+    axios
+      .get(url)
+      .then((response) => {
+        setData(response.data);
+        setError(false);
+        setLoc("");
+        setTimeout(() => {
+          setLoading(false);
+        }, 1000);
+      })
+      .catch((error) => {
+        setError(true);
+        setLoading(false);
+      });
+  };
+
+  const handleKeyPress = (event) => {
+    if (event.key === "Enter") {
+      searchLocation();
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="search-box">
+        <div className="search-input-container">
+          <input
+            value={loc}
+            onChange={(event) => setLoc(event.target.value)}
+            onKeyPress={handleKeyPress}
+            placeholder="Enter Your Location"
+            type="text"
+          />
+          <button className="search-button" onClick={searchLocation}>
+            Search
+          </button>
+        </div>
+      </div>
+      <div className="container">
+        {error ? (
+          <div className="error">
+            <h1>Wrong location entered</h1>
+          </div>
+        ) : (
+          <div className="top">
+            <div className="location">
+              <p className="loactiontext" >{data.name}</p>
+            </div>
+            <div className="weather-icon">
+              {data.weather && data.weather[0] ? (
+                <img
+                  src={`http://openweathermap.org/img/w/${data.weather[0].icon}.png`}
+                  alt={data.weather[0].description}
+                />
+              ) : null}
+            </div>
+            <div className="temp">
+              {data.main ? <h1>{((data.main.temp - 32) * 5/9).toFixed()}°C</h1> : null}
+            </div>
+            <div className="description">
+              {data.weather ? (
+                <p className="bold">{data.weather[0].main}</p>
+              ) : null}
+            </div>
+          </div>
+        )}
+        {data.name !==undefined && !error && (
+          <div className="bottom">
+            <div className="feels">
+              {data.main ? (
+                <p className="bold">{((data.main.feels_like-32)*5/9).toFixed(2)}°C</p>
+              ) : null}
+              <p>Feels Like</p>
+            </div>
+            <div className="humidty">
+              {data.main ? <p className="bold">{data.main.humidity}%</p> : null}
+              <p>Humidity</p>
+            </div>
+            <div className="wind">
+              {data.wind ? <p className="bold">{data.wind.speed}MPH</p> : null}
+              <p>Wind Speed</p>
+            </div>
+          </div>
+        )}
+        {loading && (
+          <div className="loading-animation">
+          <Lottie loop={true} animationData={loadingAnimation} />
+          <h1 className="loadingtext">Loading...</h1>
+          <p className="loadingtext">Just Wait</p>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
